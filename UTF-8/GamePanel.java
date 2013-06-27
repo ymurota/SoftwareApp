f65ccb81273bbbb4ce41e7529d98a2bf4e62e0be
U-Wanwannodao\わんわんおだお <@Wanwannodao.(none)>
2013-06-27 13:27:50 +0900

    文字コードUTF-8

---
+++ UTF-8/GamePanel.java
@@ -0,0 +1,162 @@
+
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Color;
+
+import javax.swing.JPanel;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.text.DecimalFormat;
+import javax.swing.ImageIcon;
+public class GamePanel extends JPanel implements Runnable{
+	private static final int WIDTH=291;
+	private static final int HEIGHT=MainFrame.HEIGHT;
+	
+	private static final int FPS = 50;
+	private static final long PERIOD = (long)(1.0/FPS*1000000000);
+	private static long INTERVAL = 1000000000L;
+    private long calcInterval = 0L;
+    private long prevCalcTime;
+    private long frameCount = 0;
+    private double actualFPS = 0.0;
+    private int POSITION;
+	private Graphics dbg;
+	private Image dBuffer = null;
+	private DecimalFormat df = new DecimalFormat("0.0");
+	private int STATE=0;
+	private int STOP = -1;
+    public static int LEFT=0,RIGHT=1;
+    public int newHP;
+	public Thread gameLoop;
+	public GamePanel(int POSITION){
+		setLayout(null);
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		gameLoop=new Thread(this);
+		this.POSITION = POSITION;
+	}
+	public void run(){
+		long beforeTime,afterTime,timeDiff,sleepTime;
+		long overSleepTime=0L;
+		int noDelay=0;
+		beforeTime = System.nanoTime();
+		prevCalcTime = beforeTime;
+		int tmpHP;
+		while(true){
+			if(STATE==STOP)break;
+			if(POSITION==LEFT)tmpHP=MainFrame.user.HP;
+			else tmpHP=MainFrame.opponent.HP;
+			if(Math.max(tmpHP, newHP+1)==tmpHP){
+				tmpHP--;
+				update(tmpHP);
+			}else{
+				update(newHP);
+			}
+			render();
+			print();
+			
+			afterTime = System.nanoTime();
+			timeDiff = afterTime-beforeTime;
+			sleepTime = (PERIOD-timeDiff)-overSleepTime;
+			if(sleepTime>0){
+				try{
+					Thread.sleep(sleepTime/1000000L);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}
+				overSleepTime = System.nanoTime()-afterTime-sleepTime;
+			}else{
+				overSleepTime=0L;
+				if(++noDelay>=1){
+					Thread.yield();
+					noDelay=0;
+				}
+			}
+			beforeTime = System.nanoTime();
+			calcFPS();
+		}
+		
+	}
+	private void update(int i){
+		switch(STATE){
+		case 0: if(POSITION==LEFT){
+			newHP = MainFrame.user.HP;
+			}
+			else{
+				newHP = MainFrame.opponent.HP;
+			}
+				STATE=1;break;
+		case 1:
+			if(POSITION==LEFT){
+				MainFrame.user.HPUpdate(i);
+			}
+			else{
+				MainFrame.opponent.HPUpdate(i);
+			}
+		break;
+		}
+	}
+	private void render(){
+		if(dBuffer==null){
+			dBuffer = createImage(WIDTH,HEIGHT);
+			 if (dBuffer == null) {
+	                System.out.println("dBuffer1 is null");
+	                return;
+	            } else {
+	                dbg = dBuffer.getGraphics();
+	            }
+		}
+		if(POSITION==LEFT){
+			dbg.drawImage(MainFrame.gameController.background,
+					0,0,
+					WIDTH,HEIGHT,
+					0,0,
+					WIDTH,HEIGHT,
+					null);
+		}else{
+			dbg.drawImage(MainFrame.gameController.background,
+					0,0,
+					WIDTH,HEIGHT,
+					MainFrame.WIDTH-291,0,
+					MainFrame.WIDTH-291+WIDTH,HEIGHT,
+					null);
+		}
+
+		if(POSITION==LEFT){		
+			dbg.setColor(Color.WHITE);
+			dbg.drawString("FPS:"+df.format(actualFPS),15,20);
+		MainFrame.user.draw(dbg);
+		}
+		else MainFrame.opponent.draw(dbg);
+	}
+	private void print(){
+		try{
+			Graphics g = getGraphics();
+			if(g!=null&&dBuffer!=null){
+				g.drawImage(dBuffer,0,0,null);
+			}
+
+			Toolkit.getDefaultToolkit().sync();
+			if(g!=null)g.dispose();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	private void calcFPS(){
+		frameCount++;
+		calcInterval += PERIOD;
+		if(calcInterval >= INTERVAL){
+			long timeNow = System.nanoTime();
+			long actualElapsedTime = timeNow - prevCalcTime;
+			actualFPS = ((double)frameCount/actualElapsedTime)*1000000000L;
+			frameCount=0L;
+			calcInterval=0L;
+			prevCalcTime=timeNow;
+		}
+	}
+	public void newHPUpdate(int HP){
+		newHP=HP;
+	}
+	public void loopStop(){
+		STATE = STOP;
+	}
+}
---
+++ UTF-8/HomePanel.java
@@ -0,0 +1,96 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import java.awt.Font;
+
+public class HomePanel extends JPanel implements ActionListener{
+	private static final int WIDTH=MainFrame.WIDTH;
+	private static final int HEIGHT=MainFrame.HEIGHT;
+	private JButton[] button;
+	private BufferedImage icon;
+	private JLabel message;
+	private Font f1,f2;
+	public HomePanel(){
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		setLayout(null);
+		f1 = FontCreator.getFont(1);
+		f2 = FontCreator.getFont(2);
+		try{
+			icon = ImageIO.read(getClass().getResource("Button3.png"));	
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		button = new JButton[2];
+		button[0] = new JButton("START",new ImageIcon(icon));
+		button[1] = new JButton("EXIT",new ImageIcon(icon));
+		for(int i=0;i<2;i++){
+			button[i].setFont(f1.deriveFont(23.0f));	
+			button[i].setForeground(Color.WHITE);
+			button[i].setBounds(WIDTH/2-100,2*HEIGHT/3+i*50,icon.getWidth(),icon.getHeight());
+			button[i].setHorizontalTextPosition(JButton.CENTER);
+			add(button[i]);
+			button[i].addActionListener(this);
+		}
+	}
+	public void paintComponent(Graphics g){
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setFont(f2.deriveFont(30.0f));
+		g.setColor(Color.WHITE);
+		g.drawString("Your Name : ", 100, 100);
+		g.setFont(f1.deriveFont(25.0f));
+		g.drawString(MainFrame.user.userName,210,100);
+	}
+	public void actionPerformed(ActionEvent e){
+		Object action = e.getSource();
+		if(action==button[0]){
+			//SE.getSE().playSE(1);
+			MainFrame.gameController.setup();
+			MainFrame.STATE=MainFrame.START;
+			MainFrame.cl.show(MainFrame.cardPanel, "Controller");
+		}
+		else if(action==button[1]){
+			//SE.getSE().playSE(1);
+			try{
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+				SwingUtilities.updateComponentTreeUI(this);
+			}catch(Exception ex){
+				ex.printStackTrace();
+			}
+			message = new JLabel("EXIT?");
+			message.setFont(f1.deriveFont(23.0f));
+			String[] option = {"YES","NO"};
+			int OPTION =JOptionPane.showOptionDialog(this,
+					message,
+					"EXIT",
+					JOptionPane.YES_NO_OPTION,
+					JOptionPane.QUESTION_MESSAGE,
+					null,
+					option,option[0]);
+			if(OPTION==0){
+				System.exit(0);
+			}else{
+				try{
+					UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+					SwingUtilities.updateComponentTreeUI(this);
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+}
